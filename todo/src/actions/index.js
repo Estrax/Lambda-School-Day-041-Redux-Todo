51e4e7162fe5d212ab70546b0d13bfc01ab25143
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-04 16:59:05 -0500

    fixed misspelled words

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -3,20 +3,20 @@ import {ADD_TODO, REMOVE_TODO, TOGGLE_TODO} from '../constants/actionTypes';
 export const addTodo = (todo) => {
     return {
         type: ADD_TODO,
-        paload: todo
+        payload: todo
     }
 }
 
 export const removeTodo = (todo) => {
     return {
         type: REMOVE_TODO,
-        paload: todo
+        payload: todo
     }
 }
 
 export const toggleTodo = (todo) => {
     return {
         type: TOGGLE_TODO,
-        paload: todo
+        payload: todo
     }
 }
