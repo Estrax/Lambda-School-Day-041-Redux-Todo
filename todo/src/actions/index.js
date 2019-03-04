b35fd44b28c0fa0a4c17d5c3e3da9cc9c954976f
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-04 17:39:06 -0500

    updated actions - payloads

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -3,20 +3,20 @@ import {ADD_TODO, REMOVE_TODO, TOGGLE_TODO} from '../constants/actionTypes';
 export const addTodo = (todo) => {
     return {
         type: ADD_TODO,
-        payload: todo
+        payload: {todo: todo, completed: false}
     }
 }
 
-export const removeTodo = (todo) => {
+export const removeTodo = (todoId) => {
     return {
         type: REMOVE_TODO,
-        payload: todo
+        payload: todoId
     }
 }
 
-export const toggleTodo = (todo) => {
+export const toggleTodo = (todoId) => {
     return {
         type: TOGGLE_TODO,
-        payload: todo
+        payload: todoId
     }
 }
