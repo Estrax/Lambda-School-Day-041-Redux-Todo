3971ba4211b660a2290066275b5d4ca4e0ead907
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-04 20:32:40 -0500

    updated app.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Todos from './containers/todos';
+import TodoForm from './components/todoForm';
+import Header from './components/header';
+import Footer from './components/footer';
 
 class App extends Component {
-  render() {
-    return (
-      <Todos />
-    );
-  }
+	render() {
+		return (
+			<>
+				<Header />
+				<Todos />
+				<TodoForm />
+				<Footer />
+			</>
+		);
+	}
 }
 
 export default App;
