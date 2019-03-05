ea38c8f8848992376858aab3a92a3a5442b57d12
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-04 21:03:39 -0500

    added data persistence with localstorage

---
+++ todo/src/helpers/storage.js
@@ -0,0 +1,21 @@
+import initialState from "./initialState";
+
+export const save = (state) => {
+    localStorage.setItem('todos', encode(state));
+}
+
+export const load = () => {
+    if(localStorage.getItem('todos').length === 0){
+        return initialState();
+    }
+
+    return decode(localStorage.getItem('todos'));
+}
+
+function encode(str) {
+    return JSON.stringify(str);
+}
+
+function decode(str) {
+    return JSON.parse(str);
+}
--- todo/src/reducers/todos.js
+++ todo/src/reducers/todos.js
@@ -1,46 +1,37 @@
 import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../constants/actionTypes';
+import { save, load } from '../helpers/storage';
 
-const initialState = {
-	todos: [
-		{
-			id: 0,
-			text: "First todo",
-			completed: false
-		},
-		{
-			id: 1,
-			text: "Second todo",
-			completed: true
-		},
-	]
-}
+const initialState = load();
 
 export default (state = initialState, action) => {
 	switch(action.type){
 		case ADD_TODO:
 			if(action.payload.trim().length === 0) return state;
-			return {
+			save({
 				...state,
 				todos: [...state.todos, {
 					id: state.todos.length,
 					text: action.payload,
 					completed: false
 				}]
-			};
+			});
+			return load();
 		
 		case REMOVE_TODO:
-			return {
+			save({
 				...state,
 				todos: state.todos.filter(elem => elem.id !== action.payload)
-			};
+			});
+			return load();
 		
 		case TOGGLE_TODO:
-			return {
+			save({
 				...state,
 				todos: state.todos.map(elem => elem.id === action.payload ? {...elem, completed: !elem.completed} : elem)
-			};
+			});
+			return load();
 			
 		default:
-			return state;
+			return load();
 	}
 }
