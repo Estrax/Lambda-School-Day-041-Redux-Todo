77ecdd76fb9f1267f3075a6b20e61bfb43db5c0b
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-03-04 20:23:11 -0500

    updated todos reducer

--- todo/src/reducers/todos.js
+++ todo/src/reducers/todos.js
@@ -1,13 +1,44 @@
-export default (state = [], action) => {
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../constants/actionTypes';
+
+const initialState = {
+	todos: [
+		{
+			id: 0,
+			text: "First todo",
+			completed: false
+		},
+		{
+			id: 1,
+			text: "Second todo",
+			completed: true
+		},
+	]
+}
+
+export default (state = initialState, action) => {
 	switch(action.type){
-		case 'ADD_TODO':
-			return state;
+		case ADD_TODO:
+			if(action.payload.trim().length === 0) return state;
+			return {
+				...state,
+				todos: [...state.todos, {
+					id: state.todos.length,
+					text: action.payload,
+					completed: false
+				}]
+			};
 		
-		case 'REMOVE_TODO':
-			return state;
+		case REMOVE_TODO:
+			return {
+				...state,
+				todos: state.todos.filter(elem => elem.id !== action.payload)
+			};
 		
-		case 'TOGGLE_TODO':
-			return state;
+		case TOGGLE_TODO:
+			return {
+				...state,
+				todos: state.todos.map(elem => elem.id === action.payload ? {...elem, completed: !elem.completed} : elem)
+			};
 			
 		default:
 			return state;
